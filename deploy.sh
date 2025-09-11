06cbf671231b19faba6269ec8f0cd8f0712d6896
Beni Dhomi <beni@qualityaccelerators.nl>
2025-09-11 13:50:16 +0200

    Deploy hello-world image

--- deploy.sh
+++ deploy.sh
@@ -1,15 +1,16 @@
 docker build -t hello-world:latest .
+
+kubectl create namespace argocd
 kubectl apply -f k8s/deployment.yaml -n argocd
+
 git add .
 git commit -m "Deploy hello-world image"
 
-kubectl create namespace argocd
 kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
-
 kubectl apply -f k8s/argocd-app.yaml -n argocd
 
-kubectl patch svc argocd-server -n argocd -p '{"spec": {"type": "LoadBalancer"}}'
-kubectl get svc argocd-server -n argocd -o=jsonpath='{.status.loadBalancer.ingress[0].ip}'
+# kubectl patch svc argocd-server -n argocd -p '{"spec": {"type": "LoadBalancer"}}'
+# kubectl get svc argocd-server -n argocd -o=jsonpath='{.status.loadBalancer.ingress[0].ip}'
 
 argocd admin initial-password -n argocd
 # TkmE6kzOOxQYRYKd
