cd30c5dda634601bfbb25fddced855877b20023e
Beni Dhomi <beni@qualityaccelerators.nl>
2025-09-11 13:40:04 +0200

    Deploy hello-world image

--- deploy.sh
+++ deploy.sh
@@ -11,4 +11,8 @@ kubectl apply -f k8s/argocd-app.yaml -n argocd
 kubectl patch svc argocd-server -n argocd -p '{"spec": {"type": "LoadBalancer"}}'
 kubectl get svc argocd-server -n argocd -o=jsonpath='{.status.loadBalancer.ingress[0].ip}'
 
+argocd admin initial-password -n argocd
+# TkmE6kzOOxQYRYKd
+
 argocd app sync hello-world
+kubectl port-forward svc/argocd-server -n argocd 8080:443
