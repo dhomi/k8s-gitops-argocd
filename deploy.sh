44ef146cb86446298848c5e1ac1f3788b9bc14c7
Beni Dhomi <beni@qualityaccelerators.nl>
2025-09-11 12:26:54 +0200

    Deploy hello-world image

--- deploy.sh
+++ deploy.sh
@@ -6,3 +6,7 @@ git commit -m "Deploy hello-world image"
 
 kubectl create namespace argocd
 kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
+
+kubectl apply -f k8s/argocd-app.yaml -n argocd
+
+argocd app sync hello-world
